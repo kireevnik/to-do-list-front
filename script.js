13651b3300bd4f8044bbefcb97e1891917dda916
kireevnik <kireev.fortech@gmail.com>
2022-07-22 15:12:07 +0300

    functions updated, names changed

--- script.js
+++ script.js
@@ -1,183 +1,209 @@
-let arrElements = [];
-let tempStr = '';
-let input = null;
-let elementEdit = null;
-let valElementEdit = null;
+let allTasks = [];
 
 window.onload = async () => {
-  input = document.getElementsByClassName('input_text')[0];
-  input.addEventListener('change', updateVal);
-  const resp = await fetch("http://localhost:8080/allTasks", {
-    method: 'GET'
-  });
-  let result = await resp.json();
-  console.log(result);
-  arrElements = result.data;
+  try {
+    const response = await fetch("http://localhost:8080/tasks", {
+      method: 'GET'
+    });
+
+    const result = await response.json();
+    allTasks = result.data;
+    render();
+  } catch (error) {
+    showError('Ошибка получения задач');
+  }
+};
+
+
+const addTask = async () => {
+  const input = document.getElementById('add-block_input');
+  if (input === null || input.value.trim() === '') {
+    input.value = '';
+    return;
+  }
+
+  try {
+    const result = await fetch('http://localhost:8080/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        text: input.value
+      })
+    });
+    const data = await result.json();
+    allTasks.push(data);
+    input.value = '';
+    render();
+
+  } catch (error) {
+    showError('Ошибка добавления задачи');
+  }
+};
+
+const deleteTasks = async () => {
+  try {
+    await fetch('http://localhost:8080/tasks', {
+      method: 'DELETE'
+    });
+    allTasks = [];
+    render();
+
+  } catch (error) {
+    showError('Ошибка удаления задач');
+  }
+};
+
+const deleteTask = async (element) => {
+  try {
+    await fetch(`http://localhost:8080/tasks/${element._id}`, {
+      method: 'DELETE',
+    });
+    index = allTasks.indexOf(element);
+    allTasks.slice(index, 1);
+    render();
+  } catch (error) {
+    showError('Ошибка удаления задачи');
+  }
+};
+
+const changeCheckBoxTask = async (element) => {
+  try {
+    const result = await fetch(`http://localhost:8080/tasks/isCheck/${element._id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        isCheck: !element.isCheck
+      })
+    });
+    const data = await result.json();
+
+    allTasks.forEach(item => {
+      if (item._id === element._id) {
+        item.isCheck = data.isCheck;
+      }
+    });
+    render();
+  } catch (error) {
+    showError('Ошибка изменения задачи');
+  }
+};
+
+const changeTextTask = async (element) => {
+  try {
+    const input = document.querySelector('.lists_with_checkBox__input_text');
+    const result = await fetch(`http://localhost:8080/tasks/text/${element._id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        text: input.value
+      })
+    });
+    const data = await result.json();
+
+    allTasks.forEach(item => {
+      if (item._id === element._id) {
+        console.log(data.text);
+        item.text = data.text;
+      }
+    });
+    render();
+  } catch (error) {
+    showError('Ошибка удаления задачи');
+  }
+};
+
+const editTask = (element) => {
+  indexEditableTask = element._id;
+  render(indexEditableTask);
+};
+
+const showError = (error) => {
+  const window = document.createElement('p');
+  const bodyContainer = document.getElementById('forError');
+  const body = document.getElementsByTagName('body')[0];
+
+  window.innerText = error;
+  bodyContainer.className = 'container_error container';
+  window.className = 'error';
+  body.append(window);
   render();
 };
 
-const updateVal = (event) => {
-  tempStr = event.target.value;
-};
-
-
-const onButtonClick = async () => {
-  arrElements.push({
-    text: tempStr,
-    isCheck: false
-  });
-  await fetch('http://localhost:8080/newTask', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8',
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify({
-      text: tempStr,
-      isCheck: false
-    })
-  });
-
-  tempStr = '';
-  input.value = "";
-  render();
-};
-
-const onclickDeleteTabl = async () => {
-  await fetch('http://localhost:8080/deleteAllTasks', {
-    method: 'DELETE'
-  });
-
-  arrElements = [];
-  render();
-};
-
-const render = () => {
-  const lists_with_checkBox = document.getElementsByClassName('lists_with_checkBox')[0];
-  while (lists_with_checkBox.firstChild) {
-    lists_with_checkBox.removeChild(lists_with_checkBox.firstChild);
+const render = (indexEditableElement = null) => {
+  const allTasks = document.getElementById('list_with_Tasks');
+  while (allTasks.firstChild) {
+    allTasks.removeChild(allTasks.firstChild);
   };
 
-  arrElements.sort((a, b) => (a.isCheck < b.isCheck) ? -1 : 1);
-  arrElements.map((element, index) => {
-    const fullContainer = document.createElement('div');
-    const containerWithImg = document.createElement('div');
-    const container = document.createElement('div');
+  const copyAllTasks = [...allTasks];
+  copyAllTasks.sort((task, nextTask) => (task.isCheck < nextTask.isCheck) ? -1 : 1);
+  copyAllTasks.forEach(element => {
+    const task = document.createElement('div');
+    const elementsInTask = document.createElement('div');
+    const editingElements = document.createElement('div');
     const checkBox = document.createElement('input');
-    const imgDelete = document.createElement('img');
-    const imgEdit = document.createElement('img');
     const p = document.createElement('p');
+    const imageDeletingTask = document.createElement('img');
+    const imageEditingTask = document.createElement('img');
 
     p.innerText = element.text;
     checkBox.type = 'checkBox';
     checkBox.checked = element.isCheck;
-    container.className = `container_${index}`;
-    imgDelete.src = './images/delete.svg';
-    imgEdit.src = './images/edit.svg';
-    fullContainer.className = 'lists_with_checkBox_container'
+    imageDeletingTask.src = './images/delete.svg';
+    imageEditingTask.src = './images/edit.svg';
+    task.className = 'lists_with_checkBox_container'
 
-    imgDelete.onclick = () => deleteElement(element);
-    imgEdit.onclick = () => onclickEdit(element, index);
-    checkBox.onclick = () => onclickCheckBox(element, index);
+    imageDeletingTask.onclick = () => deleteTask(element);
+    imageEditingTask.onclick = () => editTask(element);
+    checkBox.onclick = () => changeCheckBoxTask(element);
 
     if (element.isCheck) {
-      container.className = 'checkBox__active';
+      editingElements.className = 'checkBox__active';
     };
 
-    if (index === elementEdit) {
-      const containerWithModuleEdit = document.createElement('div');
-      const inputText = document.createElement('input');
-      const imgEditCansel = document.createElement('img');
-      const imgEditApp = document.createElement('img');
+    if (element._id === indexEditableElement) {
+      const actionCancelAndAdding = document.createElement('div');
+      const newText = document.createElement('input');
+      const imageCancel = document.createElement('img');
+      const imageAdding = document.createElement('img');
 
-      inputText.className = 'lists_with_checkBox__input_text';
-      inputText.value = element.text;
-      imgEditCansel.src = './images/cancel.png';
-      imgEditApp.src = './images/apply.png';
+      newText.className = 'lists_with_checkBox__input_text';
+      newText.value = element.text;
+      imageCancel.src = './images/cancel.png';
+      imageAdding.src = './images/apply.png';
 
-      inputText.addEventListener('change', updateInputText);
-      imgEditApp.onclick = () => onclickEditApp(element);
-      imgEditCansel.onclick = () => onclickEditCansel();
+      imageAdding.onclick = () => changeTextTask(element);
+      imageCancel.onclick = () => render();
 
-      containerWithModuleEdit.append(imgEditCansel);
-      containerWithModuleEdit.append(imgEditApp);
-      containerWithImg.append(containerWithModuleEdit);
-      container.append(checkBox);
-      container.append(inputText);
+      actionCancelAndAdding.append(imageCancel);
+      actionCancelAndAdding.append(imageAdding);
+      elementsInTask.append(actionCancelAndAdding);
+      element.append(checkBox);
+      editingElements.append(newText);
     } else {
       let p = document.createElement('p');
       p.innerText = element.text;
 
-      container.append(checkBox);
-      container.append(p);
+      editingElements.append(checkBox);
+      editingElements.append(p);
 
       if (!element.isCheck) {
-        containerWithImg.append(imgEdit);
+        elementsInTask.append(imageEditingTask);
       };
     };
 
-    containerWithImg.append(imgDelete);
-    fullContainer.append(container);
-    fullContainer.append(containerWithImg);
-    lists_with_checkBox.append(fullContainer);
-  });
-};
-
-const deleteElement = async (element) => {
-  await fetch('http://localhost:8080/deleteTask', {
-    method: 'DELETE',
-    params: {
-      _id: element._id
-    }
-  });
-
-  arrElements.splice(arrElements.indexOf(element), 1);
-  render();
-};
-
-
-const updateInputText = (event) => {
-  arrElements[elementEdit].text = event.target.value;
-  render();
-};
-
-const onclickCheckBox = (element, index) => {
-  console.log(element);
-  pathTask(element);
-  arrElements[index].isCheck = !arrElements[index].isCheck;
-  render();
-};
-
-const onclickEditCansel = () => {
-  arrElements[elementEdit].text = valElementEdit;
-  elementEdit = null;
-};
-
-const onclickEditApp = async (element) => {
-  elementEdit = null;
-  pathTask(element);
-  render();
-};
-
-const onclickEdit = (element, index) => {
-  elementEdit = index;
-  valElementEdit = element.text;
-  render();
-};
-
-const pathTask = async (element) => {
-  await fetch('http://localhost:8080/updateTask', {
-    method: 'PATCH',
-    params: {
-      _id: element._id,
-    },
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8',
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify({
-      isCheck: !element.isCheck,
-      text: element.text
-    })
+    elementsInTask.append(imageDeletingTask);
+    task.append(editingElements);
+    task.append(elementsInTask);
+    allTasks.append(task);
   });
 };
